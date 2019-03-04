6952d59e5827db00f41255e6df59054167ffac69
zoushuai <zoushuai@git.apuscn.com>
2019-03-04 15:59:06 +0800

    first commit

--- src/apps/nlp_category/models.py
+++ src/apps/nlp_category/models.py
@@ -5,9 +5,9 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.utils import timezone
 
 
-@python_2_unicode_compatible
-class NewUser(AbstractUser):
-    profile = models.CharField('profile', default='', max_length=256)
-
-    def __str__(self):
-        return self.username
+# @python_2_unicode_compatible
+# class NewUser(AbstractUser):
+#     profile = models.CharField('profile', default='', max_length=256)
+#
+#     def __str__(self):
+#         return self.username
