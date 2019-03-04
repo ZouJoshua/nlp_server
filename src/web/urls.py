b6da9d0161e88c991c42101595a9ef32062f3391
zoushuai <zoushuai@git.apuscn.com>
2019-03-04 16:20:30 +0800

    first commit

--- src/web/urls.py
+++ src/web/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-path('nlp_category/', include('nlp_category.urls', namespace='nlp_category'))
+    path('nlp_category/', include('nlp_category.urls', namespace='nlp_category'))
 ]
