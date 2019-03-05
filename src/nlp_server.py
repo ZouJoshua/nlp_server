4e2ed3d400ff69cf031e28525c3cb0136aed55e9
zoushuai <zoushuai@git.apuscn.com>
2019-03-05 20:30:11 +0800

    first commit

---
+++ src/nlp_server.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  : Joshua
+@Time    : 2019/3/5 18:28
+@File    : nlp_server.py
+@Desc    : 
+"""
+
+from src.utils.daemonize import *
+import getopt
+import sys
+
+
+if __name__ == '__main__':
+
+    opts, args = getopt.getopt(sys.argv[1:], '-h:', ['help'])
+    if args[2]:
+        daemonize('./.nlp_server_pidfile')
+    else:
+        PORT = 1
+
+    for opt_name, opt_value in opts:
+        if opt_name in ('-h', '--help'):
+            print("[*] Help info")
+            exit()
+
+
+
---
+++ src/utils/daemonize.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+def daemonize(pidfile=None):  
+    # flush io
+    sys.stdout.flush()  
+    sys.stderr.flush()  
+    # Do first fork.  
+    try:  
+        pid = os.fork()  
+        if pid > 0: sys.exit(0) # Exit first parent.  
+    except OSError as e:
+        sys.stderr.write("fork #1 failed: (%d) %s\n" % (e.errno, e.strerror))  
+        sys.exit(1)         
+        # Decouple from parent environment.  
+        # os.chdir("/")  
+        os.umask(0)  
+        os.setsid()  
+        # Do second fork.  
+    try:  
+        pid = os.fork()  
+        if pid > 0: sys.exit(0) # Exit second parent.  
+    except OSError as e:
+        sys.stderr.write("fork #2 failed: (%d) %s\n" % (e.errno, e.strerror))  
+        sys.exit(1)
+    for f in sys.stdout, sys.stderr:
+        f.flush()
+    si = file('/dev/null', 'r')
+    so = file('./nohup.out', 'a+')
+    se = file('./nohup.out', 'a+', 0)
+    os.dup2(si.fileno(), sys.stdin.fileno())
+    os.dup2(so.fileno(), sys.stdout.fileno())
+    os.dup2(se.fileno(), sys.stderr.fileno())  
+
+    pid = str(os.getpid())  
+
+    if pidfile: file(pidfile,'w+').write("%s\n" % pid)  
