a6562bf332ee3b8c9c39c39dc70131543e52b6e5
zoushuai <zoushuai@git.apuscn.com>
2019-03-28 20:39:45 +0800

    first commit

--- src/client/client.py
+++ src/client/client.py
@@ -10,55 +10,15 @@
 import requests
 import json
 
-
-url1 = 'http://10.65.0.76:19901/nlp_category/category'
-url2 = 'http://10.65.0.76:19901/nlp_category/top'
-url3 = 'http://10.65.0.76:19901/nlp_category/sub'
-
+url = 'http://10.65.0.76:18801/nlp_regional/regional'
 
 title = "Messenger App Gets New Powerful Camera"
 content = """To help make conversations better, Facebook on Friday launched a new, faster and easier-to-use camera with art and special effects in its Messenger app that will be rolled out globally over the coming days. “We have seen that the way people are messaging is becoming much more visual. In fact, over 2.5 billion emojis, photos, stickers and videos are sent every day on Messenger,” the company said in a blog. “In some ways the camera is now replacing the keyboard. As more people use Messenger in their everyday lives, we wanted to make it faster, simpler and more fun to send photos and videos. So we built the new Messenger camera,” Facebook said. The new camera is quicker than previous versions which makes it easier for users to capture and share moments as they happen. Whether you are already in a conversation or have just opened up the app, you will see the shutter button centre in the screen. A tap takes photo and a long press records a video. The social media giant also introduced new art and special effects. “We are especially excited to debut 3D masks and special effects, which make it super easy to apply an artistic filter to your full screen photo and to turn your world into a work of art,” the company said. Facebook has also incorporated thousands of stickers, frames, masks and effects in the app. A user can also create text, add art and stickers to the text. """
-top_category = "technology"
 
-# 请求一级和二级类参数
-"""
-传入参数：
-    title: string（必传）
-    content: string（必传）
-    thresholds: （float，float）（可不传，默认参数（0.3,0.2））
-"""
-parms = {"title": title, "content": content, "top_category": top_category}
+parms = {"title": title, "content": content}
 
-# 单独请求一级类参数
-"""
-传入参数：
-    title: string（必传）
-    content: string（必传）
-    thresholds: float（可不传，默认参数0.3）
-"""
-# parms = {"title": title, "content": content, "thresholds": 0.3}
-# 单独请求二级类参数
-"""
-传入参数：
-    title: string（必传）
-    content: string（必传）
-    top_category: string（必传）
-    thresholds: float（可不传，默认参数0.2）
-"""
-# parms = {"title": title, "content": content, "top_category": top_category, "thresholds": 0.2}
 
-# category test
-resp1 = requests.post(url1, data=parms)  # 发送请求
+# regional test
+resp = requests.post(url, data=parms)  # 发送请求
 # Decoded text returned by the request
-print(resp1.text)
-
-# top_category test
-resp2 = requests.post(url2, data=parms)  # 发送请求
-# Decoded text returned by the request
-print(resp2.text)
-
-# sub_category test
-resp3 = requests.post(url3, data=parms)  # 发送请求
-# Decoded text returned by the request
-print(resp3.text)
-
+print(resp.text)
