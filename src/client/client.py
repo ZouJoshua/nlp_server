95868e5a629cec40d93f13003e507df2633f09f0
zs <joshua_zou@163.com>
2019-04-16 20:02:32 +0800

    add hi news spider parser

--- src/client/client.py
+++ src/client/client.py
@@ -14,15 +14,15 @@ url = 'http://127.0.0.1:8020/nlp_parser/parser'
 
 id1 = "1502764925793848"
 parser_url1 = "http://www.bollywoodnews.org/go/WlZTp"
-parms1 = {"id": id1, "website": parser_url1}
+parms1 = {"id": id1, "website": parser_url1, "lang": "en"}
 
 id2 = "1502776564471413"
 parser_url2 = "https://www.sciencedaily.com/releases/2019/02/190225123449.htm"
-parms2 = {"id": id2, "website": parser_url2}
+parms2 = {"id": id2, "website": parser_url2, "lang": "en"}
 
 id3 = "1502889409622478"
 parser_url3 = "https://theindianawaaz.com/president-confers-gandhi-peace-prize/"
-parms3 = {"id": id3, "website": parser_url3}
+parms3 = {"id": id3, "website": parser_url3, "lang": "en"}
 
 
 
