4ecd8de29f594f53a489423e57b2146235e78279
zs <joshua_zou@163.com>
2019-04-28 20:59:35 +0800

    add hi news result process from log

--- src/client/process_log.py
+++ src/client/process_log.py
@@ -9,18 +9,27 @@
 
 import re
 import json
-log_file = "/data/in_hi_news_parser_result/parser_server.log.2019-04-20"
-result_file = "/data/parser_server_log_result"
+
+import os
+
+base_dir = "/data/in_hi_news_parser_result"
+
+tmp_result_file = "/data/parser_server_log_result"
+ori_file = "/data/in_hi_html_random.json"
+log_file = os.path.join(base_dir, "parser_server.log.2019-04-22")
+final_result_file = os.path.join(base_dir, "hi_news_parser_20190421")
 
 
-def ger_result_from_log():
+
+def ger_result_from_log(tmp_result_file):
     patten1 = re.compile(r"\[Successful parse html (.*?),")
     patten2 = re.compile(r"result:(\{.*?\})")
 
     f = open(log_file, 'r')
-    lines = f.readlines()[118056:]
+    # lines = f.readlines()[118056:]
+    lines = f.readlines()
     f.close()
-    _of = open(result_file, 'w')
+    _of = open(tmp_result_file, 'w')
 
     for i in range(len(lines)):
         url = re.findall(patten1, lines[i])
@@ -36,38 +45,110 @@ def ger_result_from_log():
     _of.close()
 
 
-ger_result_from_log()
+# ger_result_from_log()
 
-ori_file = "/data/in_hi_html_random.json"
+def get_parser_result(ori_file, log_file, final_result_file):
 
-f = open(ori_file, 'r')
-lines = f.readlines()[400000:3000000]
-f.close()
-ori_dict = dict()
+    f = open(ori_file, 'r')
+    lines = f.readlines()[400000:]
+    f.close()
+    ori_dict = dict()
 
-for _line in lines:
-    line = json.loads(_line.strip())
-    if line:
-        ori_dict[line['url']] = line['id']
+    for _line in lines:
+        line = json.loads(_line.strip())
+        if line:
+            ori_dict[line['url']] = line['id']
 
-res_ = open(result_file, 'r')
+    res_ = open(log_file, 'r')
 
-final_result_file = "/data/in_hi_news_parser_result/hi_news_parser_20190420_log_result"
-
-out_ = open(final_result_file, 'w')
-while True:
-    r_dict = dict()
-    line_ = res_.readline().strip()
-    if line_:
-        r_line = json.loads(line_)
-        url_ = r_line['url']
-        if url_ in ori_dict.keys():
-            _id = ori_dict[url_]
-            r_dict[_id] = r_line['result']
-            r_dict['url'] = url_
-            out_.write(json.dumps(r_dict) + '\n')
+    out_ = open(final_result_file, 'w')
+    while True:
+        r_dict = dict()
+        line_ = res_.readline().strip()
+        if line_:
+            r_line = json.loads(line_)
+            url_ = r_line['url']
+            if url_ in ori_dict.keys():
+                _id = ori_dict[url_]
+                r_dict[_id] = r_line['result']
+                r_dict['url'] = url_
+                out_.write(json.dumps(r_dict) + '\n')
+            else:
+                continue
         else:
-            continue
-    else:
-        out_.close()
-        break
+            out_.close()
+            break
+
+
+def rewrite_result(file1, file2, new_file):
+    old_f1 = open(file1, 'r')
+    old_f2 = open(file2, 'r')
+    new_f = open(new_file, 'w')
+    old_list = old_f1.readlines() + old_f2.readlines()
+    old = [i.strip() for i in old_list]
+    new_list = dict()
+    for i in old:
+        line = json.loads(i)
+        url = line['url']
+        if url in new_list.keys():
+            new_list[url] += 1
+        else:
+            new_list[url] = 1
+            out = dict()
+            for k, v in line.items():
+                if k =='url':
+                    out['url'] = line[k]
+                else:
+                    out['id'] = k
+                    out['result'] = line[k]
+
+            new_f.write(json.dumps(out) + "\n")
+
+
+    old_f1.close()
+    old_f2.close()
+    new_f.close()
+
+
+def rewrite_result_v1(file1, new_file):
+    old_f1 = open(file1, 'r')
+    new_f = open(new_file, 'w')
+    old_list = old_f1.readlines()
+    old = [i.strip() for i in old_list]
+    new_list = dict()
+    s = 0
+    for i in old:
+        line = json.loads(i)
+        url = line['url']
+        if url in new_list.keys():
+            new_list[url] += 1
+        else:
+            new_list[url] = 1
+            out = dict()
+            s +=1
+            for k, v in line.items():
+                if k =='url':
+                    out['url'] = line[k]
+                else:
+                    out['id'] = k
+                    try:
+                        if type(line[k]) ==dict:
+                            out['result'] = line[k]
+                        else:
+                            out['result'] = json.loads(line[k].strip())
+                    except :
+                        print(s)
+            new_f.write(json.dumps(out) + "\n")
+
+
+    old_f1.close()
+    new_f.close()
+
+
+
+if __name__ == '__main__':
+    file1 = os.path.join(base_dir, 'hi_news_parser_20190417')
+    file2 = os.path.join(base_dir, 'hi_news_parser_20190417_')
+    file_new = os.path.join(base_dir, 'parsered_hi_news_20190417')
+    # rewrite_result(file1, file2, file_new)
+    rewrite_result_v1(file1, file_new)
